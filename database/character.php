2f95bdf0d2151a95b478f619c912743b4b9eac7b
possible_triangle <fireflyerfighter@gmail.com>
2019-06-03 12:47:30 +0200

    stats

--- database/character.php
+++ database/character.php
@@ -79,7 +79,7 @@
 		}
 		
 		public function stats() {
-			return $this->relations['clazz']->relations['stats']->toBase()->add($this->relations['race']->relations['stats']);
+			return $this->relations['clazz']->relations['stats']->add($this->relations['race']->relations['stats']);
 		}
 		
 		public function position() {
