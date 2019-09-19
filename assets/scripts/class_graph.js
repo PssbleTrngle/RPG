b5d5973b5c7fd0b9c0aab63274608bdabe2135a7
possible_triangle <fireflyerfighter@gmail.com>
2019-09-19 19:27:11 +0200

    classes reworked

--- assets/scripts/class_graph.js
+++ assets/scripts/class_graph.js
@@ -1,98 +1,29 @@
 $(window).ready(function() {
-	
-	/*
-    var nodes = new vis.DataSet([
-		{% for class in classes %}
-       		{id: {{ class.id }}, label: '{{ class.name() }}'},
-		{% endfor %}
-    ]);
-	
-    var edges = new vis.DataSet([
-		{% for class in classes %}
-			{% for to in class.evolvesTo().get() %}
-       			{from: {{ class.id }}, to: {{ to.id }}},
-			{% endfor %}
-		{% endfor %}
-    ]);
-    */
 
-    let starter_colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF8000']
-	
-    let classes = {
-       	1: {label: 'Apprentice'},
-       	2: {label: 'Fighter'},
-       	3: {label: 'Wild'},
-       	4: {label: 'Traveller'},
-       	5: {label: 'Psychic'},
-
-       	11: {label: 'Druid', from: 1},
-       	12: {label: 'Mage', from: 1},
-       	13: {label: 'Alchemist', from: 1},
-
-       	21: {label: 'Duelist', from: 2},
-       	22: {label: 'Warrior', from: [2, 3]},
-       	23: {label: 'Focused', from: 2},
-
-       	31: {label: 'Ranger', from: 3},
-       	32: {label: 'Hermit', from: 3},
-
-       	41: {label: 'Rogue', from: 4},
-       	42: {label: 'Barde', from: 4},
-       	43: {label: 'Smith', from: 4},
-
-       	51: {label: 'Priest', from: 5},
-       	52: {label: 'Medium', from: 5},
-       	53: {label: 'Telepath', from: 5},
-
-       	101: {label: 'Shaman', from: [11, 32]},
-       	102: {label: 'Necromancer', from: [11, 52]},
-       	104: {label: 'Wizard', from: [12, 53]},
-       	105: {label: 'Elementalist', from: [12, 13]},
-       	106: {label: 'Infused', from: 13},
-       	107: {label: 'Ritualist', from: [13, 51]},
-
-       	201: {label: 'Rebel', from: 21},
-       	202: {label: 'Hero', from: 21},
-       	203: {label: 'Knight', from: 22},
-       	204: {label: 'Berserk', from: 22},
-
-       	301: {label: 'Tamer', from: 31},
-       	303: {label: 'Hunter', from: 31},
-       	304: {label: 'Monk', from: [32, 23]},
-
-       	401: {label: 'Assassin', from: 41},
-       	402: {label: 'Swift', from: [41, 23]},
-       	403: {label: 'Inventor', from: 43},
-       	404: {label: 'Fool', from: 42},
-       	405: {label: 'Performer', from: 42},
-
-       	501: {label: 'Cleric', from: [51, 22]},
-       	502: {label: 'Thaumaturge', from: 52},
-       	503: {label: 'Summoner', from: 52},
-
-       	1001: {label: 'Driven', from: [107, 204]},
-       	1002: {label: 'Sage', from: [104, 304]},
-       	1003: {label: 'Beast', from: [301, 107]},
-       	1004: {label: 'Fallen', from: 203},
-       	1005: {label: 'Guardian', from: 203},
-       	1006: {label: 'Chosen', from: 202},
-       	1007: {label: 'Insane', from: [404]},
-       	1008: {label: 'Narrator', from: [405]},
-    };
+    let starter_colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF8000'];
+    let classes = window.classes;
 
 	let edges = [];
     for(let id in classes) {
+
     	let node = classes[id];
     	node.id = parseInt(id);
-    	if(node.from) {
+
+    	if(node.from.length > 0) {
     		if(!Array.isArray(node.from)) node.from = [node.from];
     		node.origin = [];
 
     		for(let from of node.from) {
     			edges.push({from, to: node.id});
     			for(let origin of classes[from].origin)
-    				node.origin.push(origin);
+    				if(!node.origin.includes(origin)) node.origin.push(origin);
     		}
+
+    		node.deg = 0;
+    		for(let origin of node.origin)
+    			node.deg += node.origin;
+
+    		node.deg = node.deg / node.origin.length * Math.pow(10, Math.floor(Math.log10(node.id)));
 
     		if(node.from.length == 1) node.colorCode = classes[node.from[0]].colorCode;
     		else node.colorCode = blend_colors(classes[node.from[0]].colorCode, classes[node.from[1]].colorCode);
@@ -100,8 +31,8 @@ $(window).ready(function() {
     	} else {
     		node.colorCode = starter_colors[node.id - 1];
     		node.origin = [node.id];
+    		node.deg = 1;
     	}
-
     	
     }
 	
@@ -125,23 +56,31 @@ $(window).ready(function() {
 		    	}
 	    }
 
+		nodesArray = nodesArray.sort(function(n1, n2) {
+			return n1.deg - n2.deg;
+		});
+
+		for(let deg = 0; deg < nodesArray.length; deg++) {
+			nodesArray[deg].deg = deg + 1;
+		}
+
 		for(let node of nodesArray) {
 	    	if(node.id > 9)
 	    		node.colorCode = blend_colors(node.colorCode, '#FFFFFF', Math.floor(Math.log10(node.id)) * 0.1);
+
 	    	node.color = {background: node.colorCode, border: node.colorCode};
+	    	if(node.origin.length == 1) node.shape = 'box';
 		}
 
+		let start = [];
 		let count = [];
-		let index = [];
 		for(let node of nodesArray) {
 		  	let level = Math.floor(Math.log10(node.id));
 		  	if(!count[level]) count[level] = 0;
-		  	if(!index[level]) index[level] = 0;
-
-		  	node.index = index[level];
-
+		  	if(!start[level])
+		  		start[level] = node.deg;
+		  	node.deg -= start[level];
 		  	count[level]++;
-		  	index[level]++;
 		}
 
 		let def = !!$(this).attr('data-display');
@@ -156,16 +95,23 @@ $(window).ready(function() {
 				dragNodes: true
 			},
 			nodes: {
+				font: {
+					color: '#000',
+					size: 20
+				},
 				color: {
 					highlight: {
-						border: '#FFF'
+						background: '#FFF',
+						border: '#FFF',
 					}
 				},
-				font: { color: '#000' }
 			},
 			edges: {
 				smooth: def,
 	    		arrows: 'to',
+				color: {
+					inherit: 'both',
+				},
 			},
 			layout: {
 				hierarchical: {
@@ -186,7 +132,7 @@ $(window).ready(function() {
 					let id = node.id;
 					let level = Math.floor(Math.log10(id));
 					let radius = 200 * (4 - level) + 10;
-					let deg = 2 * Math.PI / count[level] * node.index;
+					let deg = 2 * Math.PI / count[level] * node.deg;
 
 					var x = radius * Math.cos(deg);
 					var y = radius * Math.sin(deg);
@@ -216,11 +162,10 @@ $(window).ready(function() {
 			
 			let id = e.nodes[0];
 			
-			if(id)
-				$('.classes').each(function() {
+			$('.classes').each(function() {
 
 				let network = $(this)[0].network;
-				let node = network.findNode(id);
+				let node = id ? network.findNode(id) : [];
 				network.selectNodes(node);
 
 			});
--- database/models/clazz.php
+++ database/models/clazz.php
@@ -42,6 +42,103 @@
 		public function stats() {
 			return $this->belongsTo(Stats::class, 'stats_id');
 		}
+
+		public static function registerAll() {
+
+			/*
+
+			$classes = [
+				['id' => 1, 'name' => 'apprentice'],
+				['id' => 2, 'name' => 'traveller'],
+				['id' => 3, 'name' => 'wild'],
+				['id' => 4, 'name' => 'fighter'],
+				['id' => 5, 'name' => 'psychic'],
+
+				['id' => 11, 'name' => 'druid', 'from' => [1]],
+				['id' => 12, 'name' => 'mage', 'from' => [1]],
+				['id' => 13, 'name' => 'alchemist', 'from' => [1]],
+
+				['id' => 41, 'name' => 'duelist', 'from' => [4]],
+				['id' => 42, 'name' => 'warrior', 'from' => [4, 3]],
+				['id' => 43, 'name' => 'focused', 'from' => [4]],
+
+				['id' => 31, 'name' => 'ranger', 'from' => [3]],
+				['id' => 32, 'name' => 'hermit', 'from' => [3]],
+
+				['id' => 21, 'name' => 'rogue', 'from' => [2]],
+				['id' => 22, 'name' => 'barde', 'from' => [2]],
+				['id' => 23, 'name' => 'smith', 'from' => [2]],
+
+				['id' => 51, 'name' => 'priest', 'from' => [5]],
+				['id' => 52, 'name' => 'medium', 'from' => [5]],
+				['id' => 53, 'name' => 'telepath', 'from' => [5]],
+
+				['id' => 101, 'name' => 'shaman', 'from' => [11, 32]],
+				['id' => 102, 'name' => 'necromancer', 'from' => [11, 52]],
+				['id' => 104, 'name' => 'wizard', 'from' => [12, 53]],
+				['id' => 105, 'name' => 'elementalist', 'from' => [12, 13]],
+				['id' => 106, 'name' => 'infused', 'from' => [13]],
+				['id' => 107, 'name' => 'ritualist', 'from' => [13, 51]],
+
+				['id' => 401, 'name' => 'rebel', 'from' => [41]],
+				['id' => 402, 'name' => 'hero', 'from' => [41]],
+				['id' => 403, 'name' => 'knight', 'from' => [42]],
+				['id' => 404, 'name' => 'berserk', 'from' => [42]],
+
+				['id' => 301, 'name' => 'tamer', 'from' => [31]],
+				['id' => 303, 'name' => 'hunter', 'from' => [31]],
+				['id' => 304, 'name' => 'monk', 'from' => [32, 23]],
+
+				['id' => 201, 'name' => 'assassin', 'from' => [21]],
+				['id' => 202, 'name' => 'swift', 'from' => [21, 43]],
+				['id' => 203, 'name' => 'inventor', 'from' => [23]],
+				['id' => 204, 'name' => 'fool', 'from' => [22]],
+				['id' => 205, 'name' => 'performer', 'from' => [22]],
+
+				['id' => 501, 'name' => 'cleric', 'from' => [51, 42]],
+				['id' => 502, 'name' => 'thaumaturge', 'from' => [52]],
+				['id' => 503, 'name' => 'summoner', 'from' => [52]],
+
+				['id' => 1001, 'name' => 'driven', 'from' => [107, 404]],
+				['id' => 1002, 'name' => 'sage', 'from' => [104, 304]],
+				['id' => 1003, 'name' => 'beast', 'from' => [301, 107]],
+				['id' => 1004, 'name' => 'fallen', 'from' => [403]],
+				['id' => 1005, 'name' => 'guardian', 'from' => [403]],
+				['id' => 1006, 'name' => 'chosen', 'from' => [402]],
+				['id' => 1007, 'name' => 'insane', 'from' => [204]],
+				['id' => 1008, 'name' => 'narrator', 'from' => [204]],
+				['id' => 1009, 'name' => 'imposter', 'from' => [205, 502]],
+				['id' => 1010, 'name' => 'king', 'from' => [401]]
+			];
+
+			global $capsule;
+
+			$capsule->table('evolution')->where('from', '>', 0)->delete();
+			static::where('id', '>', 1)->delete();
+
+			foreach($classes as $class) {
+
+				$object = $class;
+				if(array_key_exists('from', $object)) unset($object['from']);
+
+				static::register($object);
+
+			}
+
+			$evolutions = [];
+
+			foreach($classes as $class) {
+
+				$from = $class['from'] ?? [];
+				foreach($from as $f) $evolutions[] = ['to' => $class['id'], 'from' => $f, 'level' => 1];
+
+			}
+
+			$capsule->table('evolution')->insert($evolutions);
+
+			*/
+
+		}
 	
 	}
 	
